1e1f063a4f30b51da76130393a983e7112b1d5c4
Jacek Szpot <maligree@gmail.com>
2013-05-07 15:56:54 +0200

    Intermediate; stubbing.

--- emissary.js
+++ emissary.js
@@ -22,22 +22,54 @@ function get_current_group() {
     return v;
 }
 
-function get_feed_for($uri) {
-        
+function get_feed_for(uri) {
+    clog("in get_feed_for(uri), uri = " + uri);
+    parse_rss(uri + "?feed=rss2", render_feeds);
 }
 
-function handle_resource_response($response) {
-    clog("This is what I got: " + response);
+function handle_resource_response(response) {
+    clog("This is what I got:");
+    console.log(response);
+}
+
+function get_user_groups() {
+    osapi.groups.get().execute(function(e) {con
 }
 
 /* Fire a request for resources and pass the response to rendering function. */
-function get_wp_resources($group_id) {
+function get_wp_resources(group_id) {
     if (osapi.resources === undefined) {
         clog("osapi.resources is not defined. This won't work. Missing a <require>, maybe?");
     } else {
         /* readable, innit? */
-        osapi.resources.get({
+        osapi.resources.getResources({
             'groupId': get_current_group()
         }).execute(handle_resource_response);
     }
+}
+
+/*
+ * render_* calls
+ * 
+ */
+
+/* Render feeds in their div, hide other divs. */
+function render_feeds(feed) {
+    clog("in render_feeds():");
+    console.log(feed);
+}
+
+/* Render div with a link to newly created site. */
+function render_goto_wp(site_name) {
+    clog("in render_goto_wp()");
+}
+
+/* Render the "loading" div, with optional message. */
+function render_loading(message) {
+    clog("in render_loading()");
+}
+
+/* Render the create-a-new-site div. */
+function render_create() {
+    clog("in render_create()");
 }
